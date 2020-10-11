50af44c79902f45b93e929aa35b954575d95e1ce
YonoKaze <450076356@qq.com>
2020-10-11 18:51:56 +0800

    第一次上传：更新了数组部分

---
+++ src/main/java/com/datastructure/MyArray.java
@@ -0,0 +1,72 @@
+package com.datastructure;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ *<p>Title:数据结构-数组部分<p>	
+ *<p>Description:数组部分相关测试<p>	
+ * @author Administrator
+ * @version 2020-10-11
+ *
+ */
+public class MyArray {
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		MyNewArray array = new MyNewArray(20);
+		
+		for(int i = 0; i < 5; i++) {
+			array.addLast(i);
+		}
+		
+		System.out.println(array);
+		array.addFirst(10);
+		System.out.println(array);
+		array.addLast(20);
+		System.out.println(array);
+		array.addIndex(3, 30);
+		System.out.println(array);
+		System.out.println(array.get(3));
+		System.out.println("=====================");
+		System.out.println(array.contains(30));
+		System.out.println(array.find(30));
+		System.out.println("=====================");
+		System.out.println(array.removeFirst());
+		System.out.println(array);
+		System.out.println(array.removeLast());
+		System.out.println(array);
+		System.out.println(array.remove(2));
+		System.out.println(array);
+		
+		System.out.println("---------------------------------");
+		Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+		
+		MyNewGenerics<Integer> array1 = new MyNewGenerics<>(20);
+
+		for(int i = 0; i < 5; i++) {
+			array1.addLast(i);
+		}
+
+		System.out.println(array1);
+		array1.addFirst(10);
+		System.out.println(array1);
+		array1.addLast(20);
+		System.out.println(array1);
+		array1.addIndex(3, 30);
+		System.out.println(array1);
+		System.out.println(array1.get(3));
+		System.out.println("=====================");
+		System.out.println(array1.contains(30));
+		System.out.println(array1.find(30));
+		System.out.println("=====================");
+		System.out.println(array1.removeFirst());
+		System.out.println(array1);
+		System.out.println(array1.removeLast());
+		System.out.println(array1);
+		System.out.println(array1.remove(2));
+		System.out.println(array1);
+		
+	}
+
+}
---
+++ src/main/java/com/datastructure/Mygenerics.java
@@ -0,0 +1,94 @@
+package com.datastructure;
+
+/**
+ * 
+ *<p>Title:数据结构-泛型部分<p>	
+ *<p>Description:泛型部分相关测试<p>	
+ * @author Administrator
+ * @version 2020-10-11
+ *
+ */
+public class Mygenerics {
+	
+	/**
+	 * 
+	 *<p>Title:学生类<p>	
+	 *<p>Description:基本属性有id，name，ismale<p>	
+	 * @author Administrator
+	 * @version 2020-10-11
+	 *
+	 */
+	static class Student{
+		private int id;
+		private String name;
+		private boolean ismale;
+
+		public Student(int id, String name, boolean ismale) {
+			this.id = id;
+			this.name = name;
+			this.ismale = ismale;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public boolean isIsmale() {
+			return ismale;
+		}
+		public void setIsmale(boolean ismale) {
+			this.ismale = ismale;
+		}
+
+		@Override
+		public String toString() {
+			StringBuffer res = new StringBuffer();
+			res.append('[');
+			res.append(String.format("Student id = %d, name = %s, ismale = %b ", id, name,ismale));
+			res.append('[');
+			res.append(id);
+			res.append(',' + name + ',');
+			res.append(ismale);
+			res.append(']').append(']');
+			return res.toString();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Student student = new Student(2, "20", true);
+		System.out.println(student);
+		
+		MyNewGenerics<Student> student1 = new MyNewGenerics<Mygenerics.Student>();
+		
+		for(int i = 0; i < 6; i++) {
+			student1.addLast(new Student(i, toString(i), (Math.random() < 0.5 ? true :false)));
+		}
+		System.out.println(student1);
+		student1.addFirst(new Student(10, "qwe", false));
+		System.out.println("---------------------------------------------");
+		System.out.println(student1);
+		System.out.println("---------------------------------------------");
+		student1.removeLast();
+		student1.remove(2);
+		System.out.println(student1);
+		
+	}
+
+	private static String toString(int i) {
+		// TODO Auto-generated method stub
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(i);
+		return buffer.toString();
+	}
+
+}
