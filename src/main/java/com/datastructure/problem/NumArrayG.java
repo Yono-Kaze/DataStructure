8badc8ad191c1f46430f394cbcc8962b46930d5c
YonoKaze <450076356@qq.com>
2020-11-18 17:15:02 +0800

    线段树部分完成

--- src/main/java/com/datastructure/problem/NumArrayG.java
+++ src/main/java/com/datastructure/problem/NumArrayG.java
@@ -1,5 +1,7 @@
 package com.datastructure.problem;
 
+import com.datastructure.segmenttree.SegmentTree;
+
 /**
  * 
  *<p>Description:307. Range Sum Query - Mutable<p>	
@@ -9,36 +11,32 @@ package com.datastructure.problem;
  */
 public class NumArrayG {
 
-	private int[] data;
-	private int[] sum; //sum[i]存储前i个元素的和，sum[0] = 0 ,sum[i] 存储nums[0....i-1]的和
-
-	public NumArrayG(int[] nums) {
-		
-		data = new int[nums.length];
-		
-		for (int i = 1; i < nums.length; i++) {
-			data[i] = nums[i];
+	private SegmentTree<Integer> segmentTree;
+	
+    public NumArrayG(int[] nums) {
+        
+    	if (nums.length > 0) {
+			Integer[] data = new Integer[nums.length];
+			
+			for (int i = 0; i < nums.length; i++) {
+				data[i] = nums[i];
+			}
+			
+			segmentTree = new SegmentTree<Integer>(data, (a,b) -> a+b);
 		}
-
-		sum = new int[nums.length + 1];
-		sum[0] = 0;
-
-		for (int i = 1; i < sum.length; i++) {
-			sum[i] = sum[i - 1] + nums[i - 1];
-		}
-	}
-
-	public void update(int index, int val) {
-
-		data[index] = val;
-
-		for (int i = index + 1; i < sum.length; i++) {
-			sum[i] = sum[i - 1] + data[i - 1];
-		}
-	}
-
-	public int sumRange(int i, int j) {
-		return sum[j + 1] - sum[i];
-	}
+    }
+    
+    public void update(int i, int val) {
+    	if(segmentTree == null) {
+    		throw new IllegalArgumentException("segmentTree is null");
+    	}
+    	
+    	segmentTree.set(i, val);
+    }
+    
+    public int sumRange(int i, int j) {
+    	
+		return segmentTree.query(i, j);
+    }
 
 }
